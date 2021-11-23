f19fe210de33f5396a81fbf74bd1b48bb33cf9e5
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2021-11-23 14:19:58 -0300

    Add files via upload

---
+++ Projeto code internet/script.js
@@ -0,0 +1,12 @@
+function abrir(id){
+
+    document.getElementById(id).style.display = "block";
+    
+    
+    }
+  
+    function fechar(id){
+  
+      document.getElementById(id).style.display = "none";
+      
+      } 
